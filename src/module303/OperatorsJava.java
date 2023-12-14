d74722c6f35099d653094aad2ee171ebaf02970c
Nurseit Kalbaev <kalban1991@gmail.com>
2023-12-14 13:01:43 -0800

    Operators in java

---
+++ src/module303/OperatorsJava.java
@@ -0,0 +1,39 @@
+package module303;
+
+public class OperatorsJava {
+    public static void main(String[] args) {
+        double a = 100, b= 10.1;
+        double v ;
+
+        v = a + b;
+
+        v = a- b;
+
+        v = a*b;
+
+        v = a%b;
+
+        System.out.println(v);
+
+        int c = 3, d = 4;
+        boolean r;
+
+        //instanceof
+
+        Animal myAnimal = new Animal();
+        System.out.println(myAnimal instanceof Object);
+
+        r = ((a < b) && (c > d) ) && (a < b) || (c < d);
+
+        int x = 2;
+        int y = 3;
+        int result = x++;
+        System.out.println("y : "+y +" result : " +  result +" x : "+ x);
+
+
+
+    }
+
+
+
+}
